e125f87b142905fd7e94f4f2171c27681df58af1
Victoria Vignon <vignonvictoria@gmail.com>
2025-11-06 17:31:43 +0100

    first commit setup back

---
+++ furniture-platform/src/main/java/com/projet/furniture_platform/FurniturePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.projet.furniture_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurniturePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurniturePlatformApplication.class, args);
+	}
+
+}
---
+++ furniture-platform/src/test/java/com/projet/furniture_platform/FurniturePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.furniture_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniturePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
